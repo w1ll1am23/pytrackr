8e3249dfbb50c9bf4249ae9a02b2b6fca40aebbe
William Scanlon <wjs.scanlon@gmail.com>
2016-12-19 13:45:56 -0500

    Updated version

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change log
+
+## 0.0.2
+- Use the same format for last seen time and last updated time
+
+## 0.0.1
+- Initial support for TrackR devices.
--- src/setup.py
+++ src/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pytrackr',
-      version='0.0.1',
+      version='0.0.2',
       description='Interface to the Trackr API',
       url='http://github.com/w1ll1am23/pytrackr',
       author='William Scanlon',
